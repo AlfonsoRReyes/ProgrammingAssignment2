ac81bb9e456467b67bf0ca9650c9b8d8f999795c
alfonso zbook <zbook@reyesaguayo.com>
2016-10-20 20:37:56 -0500

    copying basic functions makeVector() and cachemean(). working fine.

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,47 @@
 
 ## Write a short comment describing this function
 
+makeVector <- function(x = numeric()) {
+  # The first function, makeVector creates a special “vector”, which is really 
+  # a list containing a function to:
+  
+  # set the value of the vector
+  # get the value of the vector
+  # set the value of the mean
+  # get the value of the mean
+  # copied from the README.md
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  # The following function calculates the mean of the special “vector” created with 
+  # the above function. However, it first checks to see if the mean has already been 
+  # calculated. If so, it gets the mean from the cache and skips the computation. 
+  # Otherwise, it calculates the mean of the data and sets the value of the mean in 
+  # the cache via the setmean function.
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+
 makeCacheMatrix <- function(x = matrix()) {
   # This function creates a special "matrix" object that can cache its inverse
 
